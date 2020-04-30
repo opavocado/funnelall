a89c6cb0a33a0daf64ff8e0ef415093076576301
Nick <nicolasverdicchio@gmail.com>
2020-04-30 03:57:32 -0300

    DropSpawner converted to Scene. Updated player sprite

--- scripts/Sandbox.gd
+++ scripts/Sandbox.gd
@@ -1,27 +1,8 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var apply_position = $Gold.position
-	var target_position = Vector2($Gold.position.x-200, $Gold.position.y-100)
-	#$Gold.apply_impulse(apply_position, target_position)
-	$Gold.add_central_force(target_position)
-	$Timer.start()
-	
+	$DropSpawner.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Timer_timeout():
-	$Timer.stop()
-	print("Timeout!")
-	$Timer.start()
+func _process(delta):
+	pass
